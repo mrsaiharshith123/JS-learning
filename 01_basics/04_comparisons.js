4a4e3cbba423d6c60af2f96777599656af458a7a
mrsaiharshith1 <mrsaiharshith1@gmail.com>
2024-11-18 13:17:40 +0530

    comparisons

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,20 @@
+// console.log(2==2)
+// console.log(2>2)
+// console.log(2<2)
+// console.log(2>=2)
+// console.log(2<=2)
+// console.log(2!=2)
+
+console.log("2" < 2);
+console.log("2" == 2);
+
+console.log(null >= 0);
+console.log(null > 0);
+console.log(null == 0);
+
+console.log(undefined == 0);
+
+//  === strict check edo okati
+
+console.log("2" === 2);
+console.log("2" === "2");
