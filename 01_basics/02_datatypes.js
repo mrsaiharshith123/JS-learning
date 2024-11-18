72a7535941e5910ff0bda6c45f56f275be65b06b
mrsaiharshith1 <mrsaiharshith1@gmail.com>
2024-11-18 12:02:46 +0530

    done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+let name = "sai";
+let age = 15;
+let isAdult = false;
+
+// string => "" or ''
+//number 2 to power of 53 is range
+//bigint (rarely used )
+//null
+// boolean => either true/false
+// undefined ..
+
+// examples:
+
+let string = "harshith";
+let number = "8000";
+let boolean = true;
+let myBrain = null; //null
+let app; // undefined
+let symbol = Symbol("hello!"); //symbol
+
+//these are primitive data types more are below
+
+//object
+let object = {
+  fullName: "sai",
+  surName: "polampalli",
+};
+
+// use type of to know which type is it
