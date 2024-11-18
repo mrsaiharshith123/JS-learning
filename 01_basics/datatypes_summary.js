a30d10afeecdcb94b1ba9bdcbd75cd9ae728e8dd
mrsaiharshith1 <mrsaiharshith1@gmail.com>
2024-11-18 14:16:40 +0530

    datatypes_summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,62 @@
+// primitve data types
+
+// 7 (thala) types: string,number,Boolean,null,undefined,bigInt,symbol
+
+const score = 77;
+const scoreValue = 77.77;
+
+const isMsdian = true;
+
+constrcbCups = null;
+
+// const cskAura = undefined; or
+let cskAura;
+
+const umm = Symbol("124");
+const agh = Symbol("124");
+
+console.log(umm === agh);
+
+const bigNum = 212399421492184284129484n;
+console.log(typeof bigNum);
+
+// Reference or non pirmitive
+
+// Array,Object,Function
+
+// Array
+
+let players = [
+  "Dhoni",
+  "Ruturaj Gaikwad",
+  "Shivam Dube",
+  "Pathirana",
+  "Jadeja",
+];
+
+// Object
+
+let unCapped = {
+  name: "MS.Dhoni",
+  age: 43,
+  price: "4CR",
+};
+
+// function
+
+function Umm() {
+  console.log("ummm");
+}
+
+// assignment 1
+/*
+string => String
+number => Number
+boolean => Boolean
+bigint =>  BigInt
+null => Object
+undefined => undefined
+Array => Object
+Function => function
+object => Object
+*/
